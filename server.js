976174e7bc5cda5ff9bc7a8964f43b323ec263b4
arefin5 <arefintalukder5@gmail.com>
2021-11-21 00:53:31 +0600

    join part

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const app = express();
+const {
+	addUser,
+	removeUser
+}=require('./user')
 const httpServer = http.createServer(app);
 // const io=socketIO(httpServer)
 const cors = require("cors");
 const port = 4000;
-// 
+//
 const io = require("socket.io")(httpServer, {
 	cors: {
 	  origin: "http://localhost:3000",
@@ -16,19 +20,21 @@ const io = require("socket.io")(httpServer, {
 	}
   });
   app.get("/", (req, res) => res.send("Hello World!"));
-
 io.on('connection',(socket)=>{
 	console.log('new user connection' , socket.id) 
-	socket.on("join",({name,room})=>{
+	socket.on("join",({name,room},callback)=>{
 		console.log("user name and room " , name,room)
+		const {error,user}=addUser({id:socket.id,name:name,room:room});
+		if(error){
+			callback(error)
+		}
+		callback()
 	})
-
-
 	socket.on('disconnect',()=>{
-		console.log("user has left")
+		console.log("user has left",socket.id)
+		removeUser(socket.id)
 	})
 })
-
 
 httpServer.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
--- user.js
+++ user.js
@@ -2,8 +2,24 @@ const users=[]
 const addUser=({name,room,id})=>{
   name=name.trim().toLowerCase();
   room=room.trim().toLowerCase();
- const existingUser=users.find(users=>user.name===name && user.room===room);
+ const existingUser=users.find(user=>user.name===name && user.room===room);
 //   
-const 
+    
+if(existingUser){
+return { error:"user alreday exist "}
 }
-const removeUser=()=>{}
+const user={id,name,room}
+users.push(user)
+return user ;
+}
+const removeUser=()=>{
+  const index=users.findIndex(user=>user.id===id)
+  if(index !==-1){
+    users.splice(index,1)[0]
+  }
+}
+
+module.exports = {
+	addUser,
+	removeUser
+};
